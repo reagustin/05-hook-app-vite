cc6709a7d10d4f4243c81338fd138b040be7a2e0
Agustin <re_agustin@hotmail.com>
2022-07-02 20:08:05 -0300

    foto de julio 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { SimpleForm } from './02-useEffect/SimpleForm'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <SimpleForm /> 
+  // </React.StrictMode>
 )
