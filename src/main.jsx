dc0077390b7ff0211f7728b25e8f6bcfc67b0391
Agustin <re_agustin@hotmail.com>
2022-07-09 23:16:46 -0300

    ejercicio

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook /> 
+    <Padre /> 
   // </React.StrictMode>
 )
 
